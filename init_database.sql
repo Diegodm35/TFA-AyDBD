c6c0e00d5b762f5f0ef581395d640d5864679b14
juanmdom <juanmdom@gmail.com>
2023-01-08 14:23:17 +0000

    script creando la base de datos

--- init_database.sql
+++ init_database.sql
@@ -1,9 +1,14 @@
 /* Borramos todas las tablas */
 DROP TABLE IF EXISTS Gimnasio; 
-DROP TABLE IF EXISTS Usuario; 
+DROP TABLE IF EXISTS Clase;
+DROP TABLE IF EXISTS Pago;
+DROP TABLE IF EXISTS Alimentacion;
+DROP TABLE IF EXISTS Accesorio;
+DROP TABLE IF EXISTS Merchandising;
+DROP TABLE IF EXISTS Producto CASCADE;
 DROP TABLE IF EXISTS Monitor; 
 DROP TABLE IF EXISTS Socio;
-DROP TABLE IF EXISTS Clase;
+DROP TABLE IF EXISTS Usuario CASCADE; 
 
 /* 
  * Tabla Gimnasio: registra los diferentes ginmasios de la misma dueña,
@@ -72,11 +77,56 @@ CREATE TABLE IF NOT EXISTS Pago (
  * monitor que imparte dicha clase
  */
 
- /* 
+CREATE TABLE IF NOT EXISTS Clase (
+   deporte VARCHAR(15) PRIMARY KEY,
+   dni_monitor VARCHAR(9) NOT NULL,
+   horario TIME NOT NULL,
+   FOREIGN KEY (dni_monitor) REFERENCES Monitor (dni_monitor)
+);
 
- CREATE TABLE IF NOT EXISTS Clase (
-    deporte VARCHAR(15) PRIMARY KEY,
-    dni_monitor VARCHAR(9) NOT NULL,
-    horario TIME NOT NULL,
-    FOREING KEY (dni_monitor) REFERENCES MONITOR (dni)
- ); */
+/* 
+* Tabla Producto: registra una serie de productos que el gimnasio ofrece
+* a la venta a sus clientes.
+*/
+
+CREATE TABLE IF NOT EXISTS Producto (
+   prod_id INT PRIMARY KEY,
+   stock INT NOT NULL,
+   nombre VARCHAR(20) NOT NULL,
+   fabricante VARCHAR(15),
+   precio FLOAT NOT NULL
+);
+
+/* 
+ * Tabla Alimentación: registra los productos que tienen que ver con la 
+ * alimentación, como barritas, batidos, etc.
+ */
+
+CREATE TABLE IF NOT EXISTS Alimentacion (
+   prod_id INT PRIMARY KEY,
+   nombre VARCHAR(20) NOT NULL,
+   FOREIGN KEY (prod_id) REFERENCES Producto (prod_id)
+);
+
+/* 
+ * Tabla Accesorio: registra los productos que tienen que ver con los 
+ * accesorios de entrenamiento, como straps, cinturones, etc.
+ */
+
+CREATE TABLE IF NOT EXISTS Accesorio (
+   prod_id INT PRIMARY KEY,
+   nombre VARCHAR(20) NOT NULL,
+   FOREIGN KEY (prod_id) REFERENCES Producto (prod_id)
+);
+
+/* 
+ * Tabla Merchandising: registra los productos que tienen que ver con los 
+ * artículos de merchandising del gimnasio, como camisetas, pulseras, etc.
+ */
+
+CREATE TABLE IF NOT EXISTS Merchandising (
+   prod_id INT PRIMARY KEY,
+   nombre VARCHAR(20) NOT NULL,
+   talla VARCHAR(10),
+   FOREIGN KEY (prod_id) REFERENCES Producto (prod_id)
+);
