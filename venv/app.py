9fa98c640af7404ad56b9eed6c624f467ea9d467
juanmdom <juanmdom@gmail.com>
2023-01-09 19:50:46 +0000

    index y about

--- venv/app.py
+++ venv/app.py
@@ -6,19 +6,23 @@ app = Flask(__name__)
 
 def get_db_connection():
     conn = psycopg2.connect(host='localhost',
-        	database="TFA_Gimnasio",
-        # user=os.environ['DB_USERNAME'],
-		user="postgres",
-		# password=os.environ['DB_PASSWORD']
-    password="1234")
+        	database="tfa_gimnasio",
+            # user=os.environ['DB_USERNAME'],
+            user="postgres",
+            # password=os.environ['DB_PASSWORD']
+            password="1234")
     return conn
 
 @app.route('/')
 def index():
-    # conn = get_db_connection()
-    # cur = conn.cursor()
-    # cur.execute('SELECT * FROM books;')
-    # books = cur.fetchall()
-    # cur.close()
-    # conn.close()
-    return "Hola rey"
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM Gimnasio;')
+    vals = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', vals=vals)
+
+@app.route('/about/', methods=('GET', 'POST'))
+def about():
+    return render_template('about.html')
