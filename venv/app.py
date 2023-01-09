bdfd6453f5219dacc733d479cf103e9474c605b7
juanmdom <juanmdom@gmail.com>
2023-01-09 19:08:59 +0000

    empezando la API

---
+++ venv/app.py
@@ -0,0 +1,24 @@
+import os
+import psycopg2
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+        	database="TFA_Gimnasio",
+        # user=os.environ['DB_USERNAME'],
+		user="postgres",
+		# password=os.environ['DB_PASSWORD']
+    password="1234")
+    return conn
+
+@app.route('/')
+def index():
+    # conn = get_db_connection()
+    # cur = conn.cursor()
+    # cur.execute('SELECT * FROM books;')
+    # books = cur.fetchall()
+    # cur.close()
+    # conn.close()
+    return "Hola rey"
